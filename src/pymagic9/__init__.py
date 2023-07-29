78973fa80bd5148d2c14099431c17509240ea239
sam <samnazarov@gmail.com>
2023-07-29 22:31:32 +0300

    Bump project version to `0.1.0rc` and other small upgrades

--- src/pymagic9/__init__.py
+++ src/pymagic9/__init__.py
@@ -3,5 +3,8 @@ PyMagic9 - a library that uses frames to analyze a call stack.
 """
 from .pymagic9 import getframe, isfunctionincallchain, nameof
 
+__author__ = "Sam Nazarov"
+__version__ = "0.1.0rc"
+
 # noinspection SpellCheckingInspection
 __all__ = ['getframe', 'isfunctionincallchain', 'nameof']
