f2288a78359a0428315408c0e230cb5fe98384cd
sam <samnazarov@gmail.com>
2023-07-30 02:56:50 +0300

    Fix author

--- src/pymagic9/__init__.py
+++ src/pymagic9/__init__.py
@@ -3,7 +3,6 @@ PyMagic9 - a library for analyzing call stacks using frames.
 """
 from .pymagic9 import getframe, isfunctionincallchain, nameof
 
-__author__ = "Sam Nazarov"
 __version__ = "0.1.0rc"
 
 # noinspection SpellCheckingInspection
