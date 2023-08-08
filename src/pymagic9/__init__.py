983e30b21e4c6feb790cfbb3a876ae578cfeb78d
sam <samnazarov@gmail.com>
2023-08-09 00:44:58 +0300

    Add `__author__` in pymagic9 `__init__`

--- src/pymagic9/__init__.py
+++ src/pymagic9/__init__.py
@@ -3,7 +3,8 @@ PyMagic9 - a library for analyzing call stacks using frames.
 """
 from .pymagic9 import getframe, isfunctionincallchain, nameof
 
-__version__ = "0.1.0rc"
+__author__ = 'Sam Nazarov'  # Duplicate in setup.cfg
+__version__ = '0.1.0rc'
 
 # noinspection SpellCheckingInspection
 __all__ = ['getframe', 'isfunctionincallchain', 'nameof']
