c3110f0bd789240dbdc577720641217cc4f8b3f5
sammnnz <104736093+sammnnz@users.noreply.github.com>
2023-09-08 01:39:25 +0300

    Change development status, bump version and other small fix
    - development status: alpha
    - version: 0.9.0a
    - delete classifier `python 3.11` from `setup.cfg`

--- src/pymagic9/__init__.py
+++ src/pymagic9/__init__.py
@@ -4,7 +4,7 @@ PyMagic9 - a library for analyzing call stacks using frames.
 from .pymagic9 import getframe, isfunctionincallchain, nameof
 
 __author__ = 'Sam Nazarov'  # Duplicate in setup.cfg
-__version__ = '0.1.0'
+__version__ = '0.9.0a'
 
 # noinspection SpellCheckingInspection
 __all__ = ['getframe', 'isfunctionincallchain', 'nameof']
