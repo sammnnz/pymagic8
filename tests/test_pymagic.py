8ad7f7afd71d1c4e9ec04427d7f70baaefaf0b5b
sammnnz <104736093+sammnnz@users.noreply.github.com>
2023-10-07 17:34:52 +0300

    Write tests for `isemptyfunction`

--- tests/test_pymagic.py
+++ tests/test_pymagic.py
@@ -4,7 +4,6 @@ Tests for pymagic9.py module
 """
 import dis
 import pymagic9.pymagic9 as pm
-# noinspection PyPackageRequirements
 import pytest
 import sys
 
@@ -146,3 +145,135 @@ def test__get_last_name():
     del f_code, frame
 
     assert result == 'frame'
+
+
+def empty_function_1():
+    pass
+
+
+def empty_function_2():
+    return
+
+
+def empty_function_3():
+    return None
+
+empty_function_4 = """
+def empty_function_4():
+    ...
+"""  # noqa 305
+if sys.version_info < (3,):
+    empty_function_4 = """
+def empty_function_4():
+    return
+"""
+
+exec(empty_function_4, globals())
+
+
+def empty_function_5():  # noqa 302
+    """ docstring """
+
+
+def empty_function_6():
+    "doc"
+
+
+def empty_function_7():
+    b'Hello World!'
+    return
+
+
+def empty_function_8():
+    # comment
+    return
+
+
+def empty_function_9():
+    """ docstring """
+    return
+
+
+empty_lambda_1 = lambda x: None  # noqa 731
+
+
+def not_empty_function_1():
+    print("Hello, world!")
+
+
+def not_empty_function_2():
+    return 0
+
+
+def not_empty_function_3():
+    []
+    return
+
+
+not_empty_lambda_1 = lambda: 0  # noqa 731
+
+
+def odd_function_1(): """docstring"""; return; None;  # noqa 702
+
+
+def odd_function_2(a, c):
+    return
+    a = 2
+    b = a * 3  # noqa 841
+
+
+def odd_function_3():
+    return
+    100
+
+
+def odd_function_4(a, *args):
+    None
+    True
+    ()
+    return
+
+
+def odd_function_5():
+    100
+    return
+
+
+@pytest.mark.parametrize(("func", "name"), [
+    (empty_function_1, None),
+    (empty_function_2, None),
+    (empty_function_3, None),
+    (empty_function_4, None),
+    (empty_function_5, None),
+    (empty_function_6, None),
+    (empty_function_7, None),
+    (empty_function_8, None),
+    (empty_function_9, None),
+    (empty_lambda_1, pm.nameof(empty_lambda_1)),
+    (not_empty_function_1, None),
+    (not_empty_function_2, None),
+    (not_empty_function_3, None),
+    (not_empty_lambda_1, pm.nameof(not_empty_lambda_1)),
+    (odd_function_1, None),
+    (odd_function_2, None),
+    (odd_function_3, None),
+    (odd_function_4, None),
+    (odd_function_5, None),
+])
+def test_isemptyfunction(func, name):
+    if name is None:
+        name = func.__name__
+
+    expected = None
+    if name.startswith("not_empty"):
+        expected = False
+    elif name.startswith(("empty", "odd")):
+        expected = True
+
+    result = pm.isemptyfunction(func)
+    assert result is expected
+
+
+def test_isemptyfunction_invalid_input():
+    with pytest.raises(TypeError, match=r"\'func\' argument must be a function"):
+        pm.isemptyfunction(None)
