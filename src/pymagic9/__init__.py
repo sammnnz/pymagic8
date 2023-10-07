c43061bf5409be915d7c640b9600d55e43a40b26
sammnnz <104736093+sammnnz@users.noreply.github.com>
2023-10-07 13:55:21 +0300

    Add `isemptyfunction` in init of `pymagic9`

--- src/pymagic9/__init__.py
+++ src/pymagic9/__init__.py
@@ -1,10 +1,10 @@
 """
 PyMagic9 - a library for analyzing call stacks using frames.
 """
-from .pymagic9 import getframe, isfunctionincallchain, nameof
+from .pymagic9 import getframe, isemptyfunction, isfunctionincallchain, nameof
 
 __author__ = 'Sam Nazarov'  # Duplicate in setup.cfg
 __version__ = '0.9.0a'
 
 # noinspection SpellCheckingInspection
-__all__ = ['getframe', 'isfunctionincallchain', 'nameof']
+__all__ = ['getframe', 'isemptyfunction', 'isfunctionincallchain', 'nameof']
